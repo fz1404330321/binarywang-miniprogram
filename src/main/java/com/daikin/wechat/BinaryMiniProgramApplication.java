47d0be6ad6969e786772726bbd606d61e7a2ec09
daikincn\PB078601 <fengzuzhou>
2021-05-21 09:38:49 +0800

    集成小程序demo

---
+++ src/main/java/com/daikin/wechat/BinaryMiniProgramApplication.java
@@ -0,0 +1,13 @@
+package com.daikin.wechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinaryMiniProgramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BinaryMiniProgramApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/daikin/wechat/BinaryMiniProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daikin.wechat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinaryMiniProgramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
